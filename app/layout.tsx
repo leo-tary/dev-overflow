386edbc3177cf1fc7f8ee36cabc4420afb7c052c
Tarun Gurmukhani <leo.tarun@gmail.com>
2024-09-02 19:48:40 +0530

    made changes to layout.tsx , added static images

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,25 +5,51 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
 import React from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // console.log(
-  //   "PUBLISHABLE KEY:",
-  //   process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-  // );
-
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient",
+          hover: "text-primary-500",
+        },
+      }}
     >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <header>
             <SignedOut>
               <SignInButton />
